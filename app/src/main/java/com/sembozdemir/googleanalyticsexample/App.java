11d64fae7627184fb7aa1ba75b6a6dc7629be063
semih <semih@VEARGD00052.ZH.CORP>
2015-08-11 17:26:24 +0300

    Initial commit

---
+++ app/src/main/java/com/sembozdemir/googleanalyticsexample/App.java
@@ -0,0 +1,32 @@
+package com.sembozdemir.googleanalyticsexample;
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Logger;
+import com.google.android.gms.analytics.Tracker;
+
+import java.util.HashMap;
+
+/**
+ * Created by semih on 8/11/15.
+ */
+public class App extends Application {
+    HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
+
+    public synchronized Tracker getTracker(TrackerName trackerId) {
+
+        if (!mTrackers.containsKey(trackerId)) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            Tracker t = analytics.newTracker(R.xml.app_tracker);
+            mTrackers.put(trackerId, t);
+        }
+        return mTrackers.get(trackerId);
+    }
+
+    public enum TrackerName {
+        APP_TRACKER,
+        GLOBAL_TRACKER,
+        E_COMMERCE_TRACKER,
+    }
+}
---
+++ app/src/main/java/com/sembozdemir/googleanalyticsexample/SecondActivity.java
@@ -0,0 +1,58 @@
+package com.sembozdemir.googleanalyticsexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        // Track Second Activity
+        Tracker t = ((App) getApplication()).getTracker(App.TrackerName.APP_TRACKER);
+        t.setScreenName("Second Activity");
+        t.send(new HitBuilders.ScreenViewBuilder().build());
+
+        // button to go to Main Activity
+        Button buttonGoToMainActivity = (Button) findViewById(R.id.buttonGoToMainActivity);
+        buttonGoToMainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
